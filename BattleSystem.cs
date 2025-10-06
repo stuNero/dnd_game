77ea1a45c18c0f34b8eb62f5d430c2d872853860
stuNero <max.vemic@student.nbi-handelsakademin.se>
2025-10-06 21:02:42 +0200

    fixed constructor, add method BattleLoop

--- BattleSystem.cs
+++ BattleSystem.cs
@@ -5,21 +5,49 @@ class BattleSystem
     Entity Attacker;
     Entity Defender;
     public int turns;
+    bool BattleActive;
     public BattleSystem(Entity attacker, Entity defender)
     {
         Attacker = attacker;
         Defender = defender;
-        
-    }
-    public void InitiateBattle()
-    {
 
+        BattleActive = true;
+        if (attacker is Enemy e)
+        {
+            Console.WriteLine($"A wild {e.Name} appears!");
+        }
+        else if (defender is Enemy f)
+        {
+            Console.WriteLine($"A wild {f.Name} appears!");
+        }
+        BattleLoop();
+    }
+    public void BattleLoop()
+    {
+        while (BattleActive)
+        {
+            turns += 1;
+            if (turns % 2 != 0)
+            {
+                AttackerTurn();
+                if (!Defender.Alive) { EndBattle(); break; }
+            }
+            else
+            {
+                DefenderTurn();
+                if (!Attacker.Alive){ EndBattle();  break;}
+            }
+        }
     }
     void AttackerTurn()
     {
         
     }
     void DefenderTurn()
+    {
+
+    }
+    void EndBattle()
     {
 
     }
