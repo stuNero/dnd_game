41bb1ce15b6c06bea5be267b82a00a80c3f82787
stuNero <max.vemic@student.nbi-handelsakademin.se>
2025-10-10 10:56:10 +0200

    Change menu structure to enums

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Xml.Serialization;
+﻿using System.ComponentModel.Design;
+using System.Xml.Serialization;
 using Game;
 
 Item longsword = new Item("Longsword", "weapon");
@@ -12,71 +13,74 @@ player1.AddItem(healthPotion);
 
 Enemy goblin1 = new Enemy("Goblin Soldier", 5, 5, 2, 100, 1, 3, "Goblin", 5);
 
+Menu currentMenu = Menu.Main;
 bool running = true;
 while (running)
 {
     string choice = "";
     Console.Clear();
-    Utility.GenerateMenu(title: "Choose an option: ", choices: new[] { "Attack enemy WIP","Take Damage", "Character", "Leave" });
-    int.TryParse(Console.ReadLine(), out int input);
-    switch (input)
+    switch (currentMenu)
     {
-        case 1:
+        case Menu.Main:
+            Utility.GenerateMenu(title: "Choose an option: ", choices: new[] { "Attack enemy WIP", "Character", "Leave" });
+            int.TryParse(Console.ReadLine(), out int input);
+            switch (input)
+            {
+                case 1: currentMenu = Menu.Battle;    break;
+                case 2: currentMenu = Menu.Character; break;
+                case 3: currentMenu = Menu.Quit;      break;
+            }
+            break;
+        case Menu.Battle:
             BattleSystem battle = new BattleSystem(player1, goblin1);
+            currentMenu = Menu.Main;
             break;
-        case 2:
+        case Menu.Character:
+            CharMenu charMenu = CharMenu.None;
             Console.Clear();
-            bool check = false;
-            foreach (Item item in player1.Equipped)
-            {
-                if (item != null)
-                {
-                    if (item.Type == "weapon")
-                    {
-                        player1.Hp -= player1.Dmg;
-                        check = true;
-                        Utility.Error($"Oof.. {player1.Name} stabbed himself with {item.Name}!\nHe took {player1.Dmg} damage..");
-                        break;
-                    }
-                }
-            }
-            if (!check)
-            {
-                Utility.Error("No weapon selected");
-                break;
-            }
-            break;
-        case 3:
-            Console.Clear();
-            Utility.GenerateMenu(title: "Choose an option: ", choices: new[] { "Equip Item", "Unequip item","Inventory", "Equipped","Check stats" });
+            Utility.GenerateMenu(title: "Choose an option: ", choices: new[] { "Take Damage DEBUG","Inventory", "Equipped","Stats" });
             choice = Utility.Prompt("",clear:false);
-            if (string.IsNullOrWhiteSpace(choice)) { break; }
+            if (string.IsNullOrWhiteSpace(choice)) { currentMenu = Menu.Main;  break; }
             int.TryParse(choice, out input);
             switch (input)
             {
-                case 1:
-                    choice = Utility.Prompt(player1.CheckInventory());
-                    if (string.IsNullOrWhiteSpace(choice)) { break; }
-                    int.TryParse(choice,out input);
-                    player1.EquipItem(player1.Inventory[input-1]);
+                case 1: charMenu = CharMenu.TakeDamage; break;
+                case 2: charMenu = CharMenu.Inventory;  break;
+                case 3: charMenu = CharMenu.Equipped;   break;
+                case 4: charMenu = CharMenu.Stats;      break;
+            }
+            switch (charMenu)
+            {
+                case CharMenu.TakeDamage:
+                    Console.Clear();
+                    bool check = false;
+                    foreach (Item item in player1.Equipped)
+                    {
+                        if (item != null)
+                        {
+                            if (item.Type == "weapon")
+                            {
+                                player1.Hp -= player1.Dmg;
+                                check = true;
+                                Utility.Error($"Oof.. {player1.Name} stabbed himself with {item.Name}!\nHe took {player1.Dmg} damage..");
+                                break;
+                            }
+                        }
+                    }
+                    if (!check)
+                    { Utility.Error("No weapon selected"); break; }
                     break;
-                case 2:
-                    choice = Utility.Prompt(player1.CheckEquipped());
-                    if (string.IsNullOrWhiteSpace(choice)) { break; }
-                    int.TryParse(choice,out input);
-                    player1.UnEquipItem(player1.Equipped[input-1]);
-                    break;
-                case 3:
+                case CharMenu.Inventory:
                     choice = Utility.Prompt(player1.CheckInventory());
                     if (string.IsNullOrWhiteSpace(choice)) { break; }
                     int.TryParse(choice, out int nr);
                     Console.WriteLine(player1.Inventory[nr - 1].Info());
                     Console.ReadLine();
                     break;
-                case 4:
+                case CharMenu.Equipped:
                     Utility.Prompt(player1.CheckEquipped());
                     break;
-                case 5:
+                case CharMenu.Stats:
                     Utility.Prompt(player1.Info());
                     break;
                 default:
@@ -84,13 +88,11 @@ while (running)
                     break;
             }
             break;
-        case 4:
+        case Menu.Quit:
             choice = Utility.Prompt("Are you sure?");
             if (string.IsNullOrWhiteSpace(choice)) { break; }else { Environment.Exit(0); }
 
             break;
-        default:
-            Utility.Error("Something went wrong in menu input");
-            break;
+        default:Utility.Error("Something went wrong in menu input"); break;
     }
 }
