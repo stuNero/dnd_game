6c33bccac525088346f49ac29f509c5effd59e0e
stuNero <max.vemic@student.nbi-handelsakademin.se>
2025-11-13 11:40:07 +0100

    add EndBattle() and alive check on battle start
    for post battle options, includes player and NPC death

--- BattleSystem.cs
+++ BattleSystem.cs
@@ -10,45 +10,92 @@ class BattleSystem
     {
         Attacker = attacker;
         Defender = defender;
+        if (attacker.Alive && defender.Alive)
+        {
 
-        BattleActive = true;
-        if (Attacker is Enemy)
-        {
-            Console.WriteLine($"A wild {Attacker.Name} appears!");
+            BattleActive = true;
+            if (Attacker is Enemy)
+            {
+                Console.WriteLine($"A wild {Attacker.Name} appears!");
+            }
+            else if (Defender is Enemy)
+            {
+                Utility.PrintColor($"A wild {Defender.Name} appears!", ConsoleColor.DarkRed);
+            }
+            Console.ReadKey(true);
+            BattleLoop();
         }
-        else if (Defender is Enemy)
+        else
         {
-            Console.WriteLine($"A wild {Defender.Name} appears!");
+            if (!attacker!.Alive)
+            {
+                defender.Loot(attacker);
+            }
+            else
+            {
+                attacker.Loot(defender);
+            }
+            return;
         }
-        BattleLoop();
     }
-    public void BattleLoop()
+    void BattleLoop()
     {
         while (BattleActive)
         {
             turns += 1;
             if (turns % 2 != 0)
             {
-                AttackerTurn();
-                if (!Defender.Alive) { EndBattle(); break; }
+                Attacker.TakeTurn(Defender);
+                if (!Defender.Alive) { EndBattle(Attacker,Defender); break; }
             }
             else
             {
-                DefenderTurn();
-                if (!Attacker.Alive){ EndBattle();  break;}
+                Defender.TakeTurn(Attacker);
+                if (!Attacker.Alive){ EndBattle(Defender,Attacker);  break;}
             }
         }
     }
-    void AttackerTurn()
+    Menu EndBattle(Entity winner, Entity killed)
     {
-
-    }
-    void DefenderTurn()
-    {
-
-    }
-    void EndBattle()
-    {
-
+        Console.ReadKey(true);
+        if (winner is Player)
+        {
+            int selectedIndex = 0;
+            bool subRunning = true;
+            string[] yesNo = ["Loot", "Leave"];
+            while (subRunning)
+            {
+                Console.Clear();
+                Utility.GenerateMenu(killed.Name + "'s body is lifeless on the floor.");
+                Utility.GenerateMenuActions(selectedIndex, yesNo);
+                switch (Console.ReadKey().Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        selectedIndex--;
+                        if (selectedIndex < 0)
+                            selectedIndex = yesNo.Length - 1;
+                        break;
+                    case ConsoleKey.DownArrow:
+                        selectedIndex++;
+                        if (selectedIndex > yesNo.Length - 1)
+                            selectedIndex = 0;
+                        break;
+                    case ConsoleKey.Enter:
+                        if (yesNo[selectedIndex] == "Loot")
+                        {
+                            subRunning = false;
+                            winner.Loot(killed);
+                        }
+                        else if (yesNo[selectedIndex] == "Leave")
+                        { subRunning = false; }
+                        break;
+                }
+            }
+            return Menu.Main;
+        }
+        else
+        {
+            return Menu.GameOver;
+        }
     }
 }
